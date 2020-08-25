892fdbf1c5a40ff6f832842bbac1ddd2d11164c8
Osaro <osaroimohe99@yahoo.com>
2020-08-25 02:05:04 -0700

    created server

---
+++ src/Components/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('*', (req, res) => {
+  res.send(res.sendFile(path.join(__dirname+'/client/public/index.html')))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
